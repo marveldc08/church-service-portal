e8e5a46ce068154b28306c602fd2d7be9c3b7699
Michael Chinye <michaelchinyechinye2018@gmail.com>
2022-03-29 16:50:27 +0100

    created Contexts.js component and populated it with functions to be used in other components

---
+++ src/components/Contexts.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { createContext, useState, useEffect } from "react";
+
+const Context = createContext({
+     openSideNav: () => {},
+     collapseSideNav: () => {},
+     isOpened: true,
+});
+
+export function AccessContexts(props){
+     const [isOpenSideNav, setIsOpenSideNav] = useState(true);
+     function closeSideNav(){
+          setIsOpenSideNav(false)
+     }
+     function openSideNav(){
+          setIsOpenSideNav(true);
+     }
+
+     const value = {
+          openSideNav: openSideNav,
+          collapseSideNav: closeSideNav,
+          isOpenSide: isOpenSideNav
+     }
+     return <Context.Provider value = {value}>
+          {props.children}
+     </Context.Provider>
+}
