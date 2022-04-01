ef387b20cc458087dfb932dc867d0288cf2e9a08
Adedayo <owoyeleadedayo25@gmail.com>
2022-04-01 14:45:46 +0100

    Added login page with react routing

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,8 +4,14 @@ import './Login.css'
 const Login = () => {
     return (
         <div className="login-block">
+        <nav>
+            <div className="img">
+                <img src="./images/UIHere.jpeg" />
+            </div>
+        </nav>
             <div className="sub-main">
-                <h1>Welcome to CELZ4 Admin</h1>
+                <h3>Welcome to CELZ4 Admin</h3>
+                <h6>Kindly input your credential.</h6>
                 <input type="email" placeholder="Email" id="email" />
                 <input type="password" placeholder="Password" id="password" />
                 <button>Submit</button>
