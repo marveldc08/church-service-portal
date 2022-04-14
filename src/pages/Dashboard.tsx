1d023c7aac0b903aefe008afa902e06e6d532fac
Michael Chinye <michaelchinyechinye2018@gmail.com>
2022-04-14 15:34:26 +0100

    Refactored the current code to use Typescript.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,10 @@
 import React, {useContext, useState} from 'react'
 import Header from '../components/Header'
-import SideNav from '../components/SideNav'
+import SideNav from '../components/SideNav';
 import Stats from '../components/Stats';
 import Charts from '../components/Charts';
 import styled from "styled-components";
 import Context from '../components/Contexts';
-// import Modal from '../components/Modal';
 import './Dashboard.css';
 function Dashboard() {
   const userContext = useContext(Context);
@@ -18,7 +17,6 @@ function Dashboard() {
           <h3>Welcome</h3>
           <Stats />
           <Charts />
-          {/* <Modal /> */}
         </Content>
       </Contain>
     </Container>
@@ -27,10 +25,13 @@ function Dashboard() {
 
 export default Dashboard
 
+interface Iprops{
+  show: boolean;
+}
 const Container = styled.div` 
   position: relative;
 `
-const Contain =styled.div`
+const Contain =styled.div<Iprops>`
     position: relative;
     margin-left: ${props => props.show ? '300px':'78px'};
     height: 100vh;
