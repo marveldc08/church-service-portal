077c6bd24b82711f5ad2217b9070eb8f63d4328d
David <emekachinye09@gmail.com>
2022-04-20 09:45:08 +0100

    Modified Chart Ui

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <App />, 
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
