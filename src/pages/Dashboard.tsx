be433cd22a95572ee6d2583821a60477fb28f541
Michael Chinye <michaelchinyechinye2018@gmail.com>
2022-04-21 11:41:29 +0100

    deleted unused imports

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,12 +7,11 @@ import styled from "styled-components";
 import Context from '../components/Contexts';
 import Modal from '../components/Modal';
 import USEMODAL from '../components/USEMODAL';
-import Loader from '../components/Loader';
 import { useGet, usePost } from '../utilities/HttpConnection';
 import requests from '../utilities/requests';
 import './Dashboard.css';
 
-import { StyledComponent } from '@emotion/styled';
+
 
 function Dashboard() {
   const userContext = useContext(Context);
