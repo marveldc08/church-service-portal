f62692af67db9834c4abd9c84523be857915878a
Michael Chinye <michaelchinyechinye2018@gmail.com>
2022-06-16 17:26:24 +0100

    completed attendance report page

--- src/pages/SubmitAttendanceReport.tsx
+++ src/pages/SubmitAttendanceReport.tsx
@@ -5,15 +5,26 @@ import SideNav from '../components/SideNav';
 import Context from "../components/Contexts";
 import { AiOutlineSend } from "react-icons/ai";
 import './AttendanceReport.css'
+import { FaRegEye } from 'react-icons/fa';
+import { useNavigate} from 'react-router'
 
 function SubmitAttendanceReport() {
      const userContext = useContext(Context);
+     const navigate = useNavigate();
 
+      // Alert states
+      const [alertHeader, setAlertHeader] = useState('');
+      const [successAlert, setSuccessAlert] = useState(false);
+      const [alertContent, setAlertContent] = useState('');
+      const [alertClass, setAlertClass] = useState('');
+
+      let churchCategory = document.getElementById('selectChurch') as HTMLSelectElement;
+      let serviceType = document.getElementById('selectService') as HTMLSelectElement;
      const serviceDateRef = useRef<HTMLInputElement>();
      const numberOfMaleRef = useRef<HTMLInputElement>();
      const numberOfFemaleRef = useRef<HTMLInputElement>();
      const numberOfFirstTimerRef = useRef<HTMLInputElement>();
-     const numberOfNewConvertsRef = useRef<HTMLInputElement>();
+     const numberOfNewConvertRef = useRef<HTMLInputElement>();
      const numberOfSpiritFilledRef = useRef<HTMLInputElement>();
 
      const handleSubmitAttendanceReport = (e?: {preventDefault: () => void;}) => {
@@ -22,7 +33,7 @@ function SubmitAttendanceReport() {
         numberOfMale: numberOfMaleRef.current?.value,
         numberOfFemale: numberOfFemaleRef.current?.value,
         numberOfFirstTimer: numberOfFirstTimerRef.current?.value,
-        numberOfNewConverts: numberOfNewConvertsRef.current?.value,
+        numberOfNewConvert: numberOfNewConvertRef.current?.value,
         numberOfSpiritFilled: numberOfSpiritFilledRef.current?.value
       }
 
@@ -30,16 +41,46 @@ function SubmitAttendanceReport() {
         method: "POST",
         body: JSON.stringify(attendance),
         headers: {"content-Type" : "application/json"}
-      }).then(response => {return response.json()}).then((data) => { alert('submitted successfully')})
+      }).then(response => {return response.json()}).then((data) => { 
+        setSuccessAlert(true); setAlertClass('alert alert-success alert-dismissible display'); setAlertContent(`Attendance report has been successfully submited.`); setAlertHeader('Atendance Report Successfully submited!')
+        churchCategory.value = "";
+        serviceType.value ="";
+        serviceDateRef.current!.value = "";
+         numberOfMaleRef.current!.value = "";
+         numberOfFemaleRef.current!.value ="";
+        numberOfFirstTimerRef.current!.value ="";
+         numberOfNewConvertRef.current!.value =""
+        numberOfSpiritFilledRef.current!.value =""
+      }).catch(error =>{
+        setSuccessAlert(true); setAlertClass('alert alert-danger alert-dismissible display'); setAlertContent(`There was an error while trying to submit your report. Kindly check your network connection and try again later`); setAlertHeader('Error!')
+      })
 
      }
 
   return (
     <Container>
       <SideNav />
+      <div className= {successAlert? alertClass : "hide"}>
+            <button type="button" className="close" data-dismiss="alert" onClick= {() => {setSuccessAlert(false);}}>&times;</button>
+              <h4><b>{alertHeader}</b></h4>
+              <p>{alertContent}</p>
+      </div> 
       <Contain show={userContext.isOpened}>
         <Header />
         <Content>
+        <ButtonWrap>
+            <button
+              className="submitAttendance__button "
+              onClick={() => {
+                navigate("/attendance-reports");
+              }}
+            >
+              <span>
+                <FaRegEye />
+              </span>{" "}
+              View Reports
+            </button>{" "}
+          </ButtonWrap>
           <FormWrap>
             <h2>
               Attendance Report{" "}
@@ -50,7 +91,7 @@ function SubmitAttendanceReport() {
             <form>
               <div className="input-wrapper">
                 <label className="submitAttendance__label">Church Category</label>
-                <select className="submitAttendance__input">
+                <select className="submitAttendance__input" id='selectChurch'>
                   <option className='submitAttendance__option' value={""}>select church category</option>
                   <option className='submitAttendance__option' value={"Adult Church"}>Adult Church </option>
                   <option className='submitAttendance__option' value={"Youth Church "}>Youth Church </option>
@@ -61,7 +102,7 @@ function SubmitAttendanceReport() {
               </div>
               <div className="input-wrapper">
                 <label className="submitAttendance__label">Service Type</label>
-                <select className="submitAttendance__input">
+                <select className="submitAttendance__input" id='selectService'>
                   <option className='submitAttendance__option' value={""}>select service type</option>
                   <option className='submitAttendance__option' value={"Sunday Service"}>Sunday Service </option>
                   <option className='submitAttendance__option' value={"Mid-Week Service "}>Mid-Week Service </option>
@@ -74,28 +115,30 @@ function SubmitAttendanceReport() {
               </div>
               <div className="input-wrapper">
                 <label className="submitAttendance__label">Service Date</label>
-                <input type="date" className="submitAttendance__input" />
+                <input type="date" className="submitAttendance__input" ref={serviceDateRef} />
               </div>
               <div className="input-wrapper">
                 <label className="submitAttendance__label">Male</label>
-                <input type="number" className="submitAttendance__input" placeholder="no_ of male" min="0"/>
+                <input type="number" className="submitAttendance__input" placeholder="no_ of male" min="0" ref={numberOfMaleRef} />
               </div>
               <div className="input-wrapper">
                 <label className="submitAttendance__label">Female</label>
-                <input type="number" className="submitAttendance__input" placeholder="no_ of female" min="0"/>
+                <input type="number" className="submitAttendance__input" placeholder="no_ of female" min="0" ref={numberOfFemaleRef} />
               </div>
               <div className="input-wrapper">
                 <label className="submitAttendance__label">First Timer</label>
-               <input type="number" className="submitAttendance__input" placeholder="no_ of first timer" min="0"/>
+               <input type="number" className="submitAttendance__input" placeholder="no_ of first timer" min="0" ref={numberOfFirstTimerRef}/>
               </div>
               <div className="input-wrapper">
                 <label className="submitAttendance__label">New Convert</label>
-                <input type="number" className="submitAttendance__input" placeholder="no_ of new convert" min="0"/>
+                <input type="number" className="submitAttendance__input" placeholder="no_ of new convert" min="0" ref={numberOfNewConvertRef} />
               </div>
               <div className="input-wrapper">
-                <label className="submitAttendance__label">HolyGhost Filled</label>
-              <input type="number" className="submitAttendance__input" placeholder="no_ of spirit filled" min="0"/>
+                <label className="submitAttendance__label">Spirit Filled</label>
+              <input type="number" className="submitAttendance__input" placeholder="no_ of spirit filled" min="0" ref={numberOfSpiritFilledRef} />
               </div>
+            </form>
+            <PreButtonWrap>
               <ButtonWrap>
                 <button className="submitAttendance__button" onClick={() => {handleSubmitAttendanceReport();}}>
                   <span>
@@ -104,7 +147,7 @@ function SubmitAttendanceReport() {
                   Submit
                 </button>
               </ButtonWrap>
-            </form>
+            </PreButtonWrap>
           </FormWrap>
         </Content>
       </Contain>
@@ -164,6 +207,12 @@ const FormWrap = styled.div`
       }
   }
 `;
+const PreButtonWrap = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 50%;
+`
 const ButtonWrap = styled.div` 
     display: flex;
         justify-content: flex-end;
